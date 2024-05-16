7ae3a520c1aeb5946951b1221a0bc016029031fb
minsooC <soochomin@gmail.com>
2024-05-16 19:23:35 +0900

    gitbash 에러를 위한 수정

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,12 +1,13 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+import dotenv, { config } from "dotenv";
 
 dotenv.config();
+
 const { MONGODB } = process.env;
 
 const connect = () => {
   mongoose
-    .connect(MONGODB, {
+    .connect(config.MONGODB, {
       dbName: "shop_nbc", // shop_nbc 데이터베이스명을 사용합니다.
     })
     .then(() => console.log("MongoDB 연결에 성공하였습니다."))
